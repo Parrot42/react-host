1e51dc35ab218fcdb6ea3db996c3676631371345
Tilman <tilman.senf@outlook.com>
2025-05-22 20:43:24 +0200

    userlist selfdelete fix

--- src/pages/admin/userList/components.jsx
+++ src/pages/admin/userList/components.jsx
@@ -3,14 +3,20 @@ import React, { useState } from "react";
 function User(props) {
     const [user, setUser] = useState(props.user);
 
+    const isAdmin = user.user_metadata.admin_key !== undefined;
+
     return (
-        <div className="user">
+        <div className={isAdmin ? "user admin" : "user"}>
             <div className="user-info">
                 <div className="user-email">{user.email}</div>
                 <div className="user-name">{user.user_metadata.display_name} {user.user_metadata.name}</div>
             </div>
             <div className="user-actions">
-                <button className="deleteUserBtn" onClick={() => props.deleteUser(user.id, user.email)}>Benutzer löschen</button>
+                {isAdmin ?
+                    <></>
+                    :
+                    <button className="deleteUserBtn" onClick={() => props.deleteUser(user.id, user.email)}>Benutzer löschen</button>
+                }
             </div>
         </div>
     );
